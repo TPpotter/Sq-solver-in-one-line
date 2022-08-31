c8f12a8ce8d2a2aa890c6195209f509db36caa93
TPpotter <80740916+TPpotter@users.noreply.github.com>
2022-08-31 14:03:37 +1000

    now you can make a pure quadratic besides quadratic equation

--- square_solver.py
+++ square_solver.py
@@ -1,8 +1,11 @@
 while True:
     try:
-        print('error' if (not (a := float(input())) or not (b := float(input())) or not (c := float(input()))) else
+        print('error' if not sum([a := float(input()), b := float(input()), c := float(input())]) else
               'no x' if ((b ** 2) - (4 * a * c)) < 0
+
+              else f'x = {-c / b}' if a == 0
               else f'x1 = {(-b + (b ** 2 - 4 * a * c) ** 0.5) / (2 * a)}\n'
                    f'x2 = {(-b - (b ** 2 - 4 * a * c) ** 0.5) / (2 * a)}')
+
     except ValueError:
         print('неправильный ввод')
